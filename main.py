414e2701855485f228710ecac8d7c52adc40e287
LiangKu Lin <jerry031785@gmail.com>
2024-07-13 14:47:57 +0800

    Add first project first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+from langchain_chroma import Chroma
+from langchain.chains import RetrievalQA
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+import argparse
+import langchain
+from langchain.prompts import PromptTemplate
+
+
+langchain.debug = True
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task",default="你作用是啥?")
+args = parser.parse_args()
+
+load_dotenv()
+embeddings = OpenAIEmbeddings()
+chat =ChatOpenAI()
+
+db = Chroma(
+    persist_directory='判例db',
+    # different from db creation
+    embedding_function= embeddings,
+)
+
+prompt = PromptTemplate(template="請給我{task}此行為應依刑事訴訟法哪幾條?",input_variables=['tasks','heelo'])
+
+
+retriever = db.as_retriever()
+
+chain = RetrievalQA.from_chain_type(    
+    llm = chat, 
+    retriever = retriever,
+    # "Stuffing" (injecting) into the system message 
+    # Description 1: This is the default setting of chain_type. It just returns the "stuff that related to the user questions."
+    # Description 2: If you set it to "map_reduce", the retreive algorithm will change. For more information, please go Udemy -> https://www.udemy.com/course/chatgpt-and-langchain-the-complete-developers-masterclass/learn/lecture/40261274?start=270#reviews 
+    chain_type = "stuff",
+    chain_type_kwargs={"prompt": prompt}
+)
+
+result = chain({'task':args.task})
+print(result)
---
+++ set-database.py
@@ -0,0 +1,28 @@
+from langchain_community.document_loaders import UnstructuredXMLLoader
+from langchain.document_loaders import TextLoader
+from dotenv import load_dotenv
+from langchain.text_splitter import CharacterTextSplitter 
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+text_splitter = CharacterTextSplitter(
+   separator="\f",
+    chunk_size = 1000,
+    chunk_overlap = 0, 
+)
+loader = PyPDFLoader("./data/最高法院刑事庭具有參考價值之裁判要旨暨裁判全文（113年4月）.pdf")
+print(loader)
+docs = loader.load_and_split(text_splitter)
+print(docs)
+
+db = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    # MySQL dirctory
+    persist_directory='判例db'
+)
