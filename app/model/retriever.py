c71fc85033042285857a8041f548afe4bf0d1069
Liangku <jerry031785@gmail.com>
2024-07-17 20:51:21 +0800

    Add custom retriever

--- app/model/retriever.py
+++ app/model/retriever.py
@@ -1,23 +1,20 @@
-from typing import Any, Dict, List
 from langchain.embeddings.base import Embeddings
-from langchain.vectorstores import Chroma 
+from langchain_community.vectorstores import Chroma
 from langchain.schema import BaseRetriever
-from langchain_core.documents import Document
 
 class RedundantFilterRetriever(BaseRetriever): 
     embeddings: Embeddings 
     chroma: Chroma 
-    
     def get_relevant_documents(self, query):
         # calculate embeddings for the 'query' string
         emb = self.embeddings.embed_query(query)
         # take embeddings and feed them into that 
         # max_marginal_relevance_search_by_vector 
        
-        return self.chroma.max_marginal_relevance_search(
-            embeddings = emb , 
+        return self.chroma.max_marginal_relevance_search_by_vector(
+            embedding = emb, 
             # the tolerance of similar documents 
-            lambda_mult= 0.8,
+            lambda_mult= 0.5,
         )
     
     async def aget_relevant_documents(self):
--- app/service/prompt.py
+++ app/service/prompt.py
@@ -1,8 +1,9 @@
 import langchain
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain_openai import OpenAIEmbeddings,ChatOpenAI
 from langchain.prompts import ChatPromptTemplate,HumanMessagePromptTemplate
+from app.model.retriever import RedundantFilterRetriever
 
 def queryQuestionFromDatabase(question): 
         langchain.debug = True
@@ -14,14 +15,18 @@ def queryQuestionFromDatabase(question):
             embedding_function= embeddings,
             persist_directory='local_database',
         )
-        retriever = db.as_retriever()
+        # retriever =  RedundantFilterRetriever(
+        #     embeddings=embeddings,
+        #     chroma = db
+        # )
+        retriever= db.as_retriever()
         chain = RetrievalQA.from_chain_type(    
             llm = chat, 
             retriever = retriever,
             chain_type = "stuff",
         )
+    
 
         # Question
-        result = chain(question)
-        print("++++++++++++++++++++++++++++++++++++++++++++++++", result)
+        result = chain.run(question)
         return result
