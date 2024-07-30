f635f92c832860c9d8f7f34d675455150fd6d268
Liangku <jerry031785@gmail.com>
2024-07-30 20:03:55 +0800

    Add item getter

--- app/service/prompt.py
+++ app/service/prompt.py
@@ -5,8 +5,8 @@ from langchain.prompts import ChatPromptTemplate,HumanMessagePromptTemplate, Sys
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough,RunnableSerializable
 from langchain_core.documents import Document
-from langchain.memory import ConversationBufferMemory
-from langchain import hub
+from operator import itemgetter
+
 
 # from app.model.retriever import RedundantFilterRetriever
 import os 
@@ -21,23 +21,20 @@ def queryQuestionFromDatabase(question):
             input_variables=["question","context"],
             messages=[
                 SystemMessagePromptTemplate.from_template("Please respond to the question according to your previous answer. If you cannot find the answer in that, please respond to the question using the provided documents. The documents are {context}. All answer should be in Chinese. If the answer cannot be found in the documents, please indicate that you are don't know the answer."),
-                # Can put all previous reqesuts and answers here 
-                HumanMessagePromptTemplate.from_template("How long are you?"),
-                AIMessagePromptTemplate.from_template("I am 30 cm"),
                 HumanMessagePromptTemplate.from_template("{question}")
             ]
         )
         chain = (
             {
-                "context": db.as_retriever() | format_docs,
-                "question": RunnablePassthrough(),
+                "context": itemgetter("question")|db.as_retriever(),
+                "question": itemgetter("question"),
             }
             | prompt
             | llm
             | StrOutputParser()
         )
         # Question
-        result = chain.invoke(question)
+        result = chain.invoke({"question":question})
         # Can stroe the messages here 
 
         return result
